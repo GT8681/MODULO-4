81e548758310da664aaf59ff77d23ac2819f9562
gianni toscano <gianni.toscano@icloud.com>
2025-10-19 23:29:02 +0200

    album

---
+++ ALBUM DI FOTO/main.js
@@ -0,0 +1,35 @@
+const API_KEY = '5Knrq4dTw0AOYZ6Sy3reI5lyvu2JzoMhnOHuCkrxD0qv2vT7DoWSoAPL'; // Sostituisci con la tua API key
+const endpoint = 'https://api.pexels.com/v1/search?query= collections/mexican-food-z6lhzw1/';
+
+const getAlbums = async () => {
+    try {
+        const response = await fetch(endpoint, {
+            headers: {
+                Authorization: API_KEY
+            }
+        });
+        const data = await response.json();
+        console.log(data);
+        const gallery = document.getElementById('gallery');
+        data.photos.forEach(photo => {
+            const img = document.createElement('img');
+            img.src = photo.src.medium;
+            img.alt = photo.photographer;
+            gallery.appendChild(img);
+        });
+       
+    }
+       
+    catch (error) {
+        console.error('Errore:', error);
+    }
+
+}
+        
+gallery.innertHTML = '';
+//al click di un bottone mi carca nuove foto
+document.getElementById('loadMoreBtn').addEventListener('click', async () =>{
+ 
+    getAlbums()
+
+});
